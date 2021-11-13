448f4573facae860c99601ee3e3a0bcc6b8ed0e4
Nicolas Mattia <nicolas@nmattia.com>
2021-11-14 00:00:34 +0100

    Light cleanup

--- index.js
+++ index.js
@@ -42,7 +42,6 @@ const setTimerCurr = (timeoutId) => ({ after, delay})=> {
 
     const timer = document.querySelector(".timer");
 
-
     if (delay === 0) {
         return after();
     }
@@ -50,6 +49,8 @@ const setTimerCurr = (timeoutId) => ({ after, delay})=> {
     setElemInnerHTML({ selector: ".timer", value: `${delay}`});
     const delayPerc = delay * 100.0 / MAX_DELAY;
     timer.style.setProperty("--percent", `${delayPerc}%`);
+
+    clearTimeout(timeoutId.pop());
     timeoutId[0] = setTimeout(() => setTimerCurr(timeoutId)({ after, delay: delay - 1}), 1000);
 };
 
@@ -60,8 +61,6 @@ document.addEventListener("DOMContentLoaded", async () => {
     const response = await fetch('./words.json');
     const { words } = await response.json();
     newWord.addEventListener("click", () => {
-        console.log(timeoutId);
-        clearTimeout(timeoutId.pop());
         startWordPrepare(words, setTimerCurr(timeoutId));
     }, { passive: true });
     newWord.removeAttribute("disabled");
